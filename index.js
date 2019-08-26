d30fe011342711cb521b9a2736600d6584840c23
Chan Le You <42783812+chanleyou@users.noreply.github.com>
2019-08-27 00:17:15 +0800

    working bot

---
+++ index.js
@@ -0,0 +1,63 @@
+const Telegraf = require('telegraf')
+const moment = require('moment')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const bot = new Telegraf(process.env.API_KEY)
+
+let history = []
+
+const clear = ({ message, deleteMessage }) => {
+  history.forEach(({ message_id }) => deleteMessage(message_id))
+  history = []
+  deleteMessage(message.id)
+}
+
+const clearHistory = ({ deleteMessage }) => {
+  const clone = [...history]
+  clone.forEach(({ message_id, time }) => {
+    if (
+      moment(time)
+        .add(24, 'hours')
+        .isBefore(moment())
+    ) {
+      deleteMessage(message_id)
+      let index = history.findIndex(message => message.message_id === message_id)
+      if (index > -1) history.splice(index, 1)
+    }
+  })
+}
+
+bot.start(ctx => {
+  ctx.reply('Initialising...')
+  setInterval(() => {
+    clearHistory(ctx)
+  }, moment.duration(1, 'hours').asMilliseconds())
+})
+
+bot.hears('/clear', clear)
+
+// bot.hears('/report', ({ message, reply }) => {
+//   reply(message)
+//   reply(`Reporting: ${history.length} message(s) stored...`)
+//   history.forEach(message => {
+//     const messageJSON = JSON.stringify(message)
+//     reply(messageJSON)
+//   })
+// })
+
+bot.launch()
+
+bot.on('message', ({ message }) => history.push({ ...message, time: moment().toISOString() }))
+
+const reportBot = async bot => {
+  try {
+    const botInfo = await bot.telegram.getMe()
+    console.log(botInfo)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+reportBot(bot)
