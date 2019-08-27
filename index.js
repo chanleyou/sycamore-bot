5b98ab57a4d01aef5bd82fecf6c4f385bcb0352c
Chan Le You <42783812+chanleyou@users.noreply.github.com>
2019-08-28 02:47:19 +0800

    env scripts, proper middleware

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 const moment = require('moment')
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   const dotenv = require('dotenv')
   dotenv.config()
 }
@@ -10,34 +10,47 @@ const bot = new Telegraf(process.env.API_KEY)
 
 const MESSAGE_DELETE_HOURS = 36
 const CLEAR_INTERVAL = moment.duration(1, 'hour').asMilliseconds()
-const ADMIN_ID = 428148796
+const ADMIN_ID = parseInt(process.env.ADMIN_ID)
 
-let store = [] // TODO: persistent cache?
+let store = [] // TODO: persistent cache between bot restarts
 
-const _deleteMessage = ({ chat_id, message_id }, index) => {
-  bot.telegram.deleteMessage(chat_id, message_id)
-  store.splice(index, 1)
+// takes a comparison function with message as arg, deletes all messages that match the condition
+const deleteMessages = condition => {
+  const deleteArray = []
+  store.forEach((message, index) => {
+    if (condition(message)) {
+      const { message_id, chat_id } = message
+      bot.telegram.deleteMessage(chat_id, message_id)
+      deleteArray.push(index)
+    }
+  })
+
+  for (let i = deleteArray.length - 1; i >= 0; i--) {
+    store.splice(deleteArray[i], 1)
+  }
 }
 
-const _recordMessage = ({ message, chat }) => {
+// record the message_id and chat_id of all messages
+const recordMiddleware = ({ message, chat }, next) => {
   const { message_id } = message
   const { id: chat_id } = chat
-  // const { text } = message
-  // const { username } = message.from
-  store.push({ message_id, chat_id, /* username, text, */ timestamp: moment().toISOString() })
+  store.push({ message_id, chat_id, timestamp: moment().toISOString() })
+  next()
 }
 
+// record outgoing bot reply messages. TODO: middleware for this
 const _recordBotReply = ({ message_id, chat }) => {
   const { id: chat_id } = chat
   store.push({ message_id, chat_id, timestamp: moment().toISOString() })
 }
 
-const _clearMessages = ({ message, chat, deleteMessage }) => {
-  const clone = [...store]
-  clone.filter(({ chat_id }) => chat_id === chat.id).forEach(_deleteMessage)
-  store = []
-  deleteMessage(message.message_id)
+// clear all messages from originating chat
+const _clearMessages = ({ chat }) => {
+  const condition = message => message.chat_id === chat.id
+  deleteMessages(condition)
 }
+
+bot.on('message', recordMiddleware)
 
 bot.start(({ reply }) => {
   reply(
@@ -50,10 +63,9 @@ bot.command('clear', _clearMessages)
 bot.command('report', ({ message, reply }) => {
   const { id } = message.from
   if (id !== ADMIN_ID) {
-    reply('Sorry, only an admin (LY) can use this command.').then(_recordBotReply)
+    reply('Sorry, only big boss can use this command.').then(_recordBotReply)
     return
   }
-
   reply(`Reporting store: ${store.length} ${store.length === 1 ? 'message' : 'messages'}.`).then(
     _recordBotReply
   )
@@ -66,18 +78,14 @@ bot.command('report', ({ message, reply }) => {
   )
 })
 
-bot.on('message', _recordMessage)
-
 bot.launch()
 
+// delete all messages >36 hours old every hour
 setInterval(() => {
   const now = moment()
-  const clone = [...store]
-  clone
-    .filter(({ timestamp }) =>
-      moment(timestamp)
-        .add(MESSAGE_DELETE_HOURS, 'seconds')
-        .isBefore(now)
-    )
-    .forEach(_deleteMessage)
+  const condition = message =>
+    moment(message.timestamp)
+      .add(MESSAGE_DELETE_HOURS, 'hours')
+      .isBefore(now)
+  deleteMessages(condition)
 }, CLEAR_INTERVAL)
